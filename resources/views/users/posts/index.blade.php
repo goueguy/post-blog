5536db7ac8f2f5f205e0755f55831061bd697729
goueguy <jlagoueguy@gmail.com>
2021-04-15 21:40:07 +0000

    finish integrate component

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -2,15 +2,9 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12">
-            <div class="p-6">
-                <h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
-                <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }} and received {{ $user->receivedLikes->count() }} likes</p>
-            </div>
-
-            <div class="bg-white p-6 rounded-lg">
-                @if ($posts->count())
-                    @foreach ($posts as $post)
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            @if ($posts->count())
+                @foreach ($posts as $post)
                     <div class="mb-4">
                         <a href="{{route('users.posts',$post->user)}}" class="font-bold">{{$post->user->name}}</a> <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
                         <p class="mb-2 justify-end">{{$post->body}}</p>
@@ -39,16 +33,15 @@
                                 </form>
                             @endif
                         @endauth
-                       
+                    
                         <span>{{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}</span>
                     </div>
-                    @endforeach
+                @endforeach
 
-                    {{ $posts->links() }}
-                @else
-                    <p>{{ $user->name }} does not have any posts</p>
-                @endif
-            </div>
+                {{ $posts->links() }}
+            @else
+                <p>There are no posts</p>
+            @endif
         </div>
     </div>
 @endsection
