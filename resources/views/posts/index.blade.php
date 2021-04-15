ca48de2e064fc9ac9b5efd55e15d37045ca0f0b3
goueguy <jlagoueguy@gmail.com>
2021-04-15 21:15:32 +0000

    make user profile

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,29 +1,66 @@
 @extends('layouts.app')
+
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{route('posts')}}" method="post" class="mb-4">
-                @csrf
-                <label for="body" class="sr-only">Body</label> 
-                <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post something">
-                </textarea> 
-                @error("body")
-                    <div class="text-red-500 mt-2 text-sm">
-                    {{$message}}
+            @auth
+                <form action="{{ route('posts') }}" method="post" class="mb-4">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="body" class="sr-only">Body</label>
+                        <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post something!"></textarea>
+
+                        @error('body')
+                            <div class="text-red-500 mt-2 text-sm">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                @enderror    
-                <div>
-                    <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded-lg mt-4">Save</button>
-                </div>          
-            </form>
-            
-            @if($posts->count())
+
+                    <div>
+                        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+                    </div>
+                </form>
+            @endauth
+
+            @if ($posts->count())
                 @foreach ($posts as $post)
-                   <x-post :post="$post" />
+                <div class="mb-4">
+                    <a href="{{route('users.posts',$post->user)}}" class="font-bold">{{$post->user->name}}</a> <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
+                    <p class="mb-2 justify-end">{{$post->body}}</p>
+                        @can("delete",$post)
+                            <!-- Delete post -->
+                            <form action="{{route('posts.destroy',$post)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="p-1 text-red-500">Delete</button>
+                            </form>
+                        @endcan
+                </div>
+                <!--unlike and like posts-->
+                <div class="flex items-center">
+                    @auth
+                        @if(!$post->likedBy(auth()->user()))
+                        <form action="{{route('posts.likes',$post)}}" method="post" class="mr-1">
+                            @csrf
+                            <button type="submit" class="text-blue-500">Like</button>
+                        </form>
+                        @else
+                            <form action="{{route('posts.dislikes',$post)}}" method="post" class="mr-1">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="p-1 text-blue-500">DisLike</button>
+                            </form>
+                        @endif
+                    @endauth
+                   
+                    <span>{{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}</span>
+                </div>
                 @endforeach
+
                 {{ $posts->links() }}
             @else
-                <p>There is no posts</p>
+                <p>There are no posts</p>
             @endif
         </div>
     </div>
--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
+
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-            @if($posts->count())
-                @foreach ($posts as $post)
+        <div class="w-8/12">
+            <div class="p-6">
+                <h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
+                <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }} and received {{ $user->receivedLikes->count() }} likes</p>
+            </div>
+
+            <div class="bg-white p-6 rounded-lg">
+                @if ($posts->count())
+                    @foreach ($posts as $post)
                     <div class="mb-4">
                         <a href="{{route('users.posts',$post->user)}}" class="font-bold">{{$post->user->name}}</a> <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
                         <p class="mb-2 justify-end">{{$post->body}}</p>
@@ -35,11 +42,13 @@
                        
                         <span>{{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}</span>
                     </div>
-                @endforeach
-                {{ $posts->links() }}
-            @else
-                <p>There is no posts</p>
-            @endif
+                    @endforeach
+
+                    {{ $posts->links() }}
+                @else
+                    <p>{{ $user->name }} does not have any posts</p>
+                @endif
+            </div>
         </div>
     </div>
 @endsection
